7a2488c157a33889f48167b1e479e0ebee056883
Andrii Parkhomenko <andriy.parhomenko@gmail.com>
2021-04-12 20:55:45 +0000

    Region field

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -7,11 +7,10 @@ class ExtraInfoForm(ModelForm):
     """
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['favorite_movie'].error_messages = {
-            "required": u"Please tell us your favorite movie.",
-            "invalid": u"We're pretty sure you made that movie up.",
+        self.fields['region'].error_messages = {
+            "required": u"Please tell us where are you from.",
         }
 
     class Meta(object):
         model = ExtraInfo
-        fields = ('favorite_editor', 'favorite_movie')
+        fields = ('region')
--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -11,20 +11,39 @@ class ExtraInfo(models.Model):
     The form that wraps this model is in the forms.py file.
     """
     user = models.OneToOneField(USER_MODEL, null=True)
-    FAVORITE_EDITOR = (
-        ('vim', 'Vim'),
-        ('emacs', 'Emacs'),
-        ('np', 'Notepad'),
-        ('cat', 'cat > filename'),
+    REGION = (
+        ('ark', 'Автономна Республіка Крим'),
+        ('vinnytsia_oblast', 'Вінницька область'),
+        ('volyn_oblast', 'Волинська область'),
+        ('dnipro_oblast', 'Дніпропетровська область'),
+        ('donetsk_oblast', 'Донецька область'),
+        ('zhytomyr_oblast', 'Житомирська область'),
+        ('zakarpatska_oblast', 'Закарпатська область'),
+        ('zaporizhzhia_oblast', 'Запорізька область'),
+        ('ivanofrankivsk_oblast', 'Івано-Франківська область'),
+        ('kyiv_oblast', 'Київська область'),
+        ('kirovograd_oblast', 'Кіровоградська область'),
+        ('luhansk_oblast', 'Луганська область'),
+        ('lviv_oblast', 'Львівська область'),
+        ('mykolaiv_oblast', 'Миколаївська область'),
+        ('odessa_oblast', 'Одеська область'),
+        ('poltava_oblast', 'Полтавська область'),
+        ('rivne_oblast', 'Рівненська область'),
+        ('sumy_oblast', 'Сумська область'),
+        ('ternopil_oblast', 'Тернопільська область'),
+        ('kharkiv_oblast', 'Харківська область'),
+        ('kherson_oblast', 'Херсонська область'),
+        ('khmelnytskyi_oblast', 'Хмельницька область'),
+        ('cherkasy_oblast', 'Черкаська область'),
+        ('chernivtsi_oblast', 'Чернівецька область'),
+        ('chernihiv_oblast', 'Чернігівська область'),
+        ('kyiv_city', 'м. Київ'),
+        ('sevastopol_city', 'м. Севастополь'),
+        ('other_country', 'Інша країна'),
     )
 
-    favorite_movie = models.CharField(
-        verbose_name="Fav Flick",
-        max_length=100,
-    )
-    favorite_editor = models.CharField(
-        verbose_name="Favorite Editor",
-        choices=FAVORITE_EDITOR,
-        blank=True, 
-        max_length=5,
+    region = models.CharField(
+        verbose_name="Region",
+        choices=REGION,
+        max_length=21,
     )
